20bd2c25373a0852a2cd5b7cd5a1b85e092a781c
Mario Serrano <maser84@gmail.com>
2022-11-28 18:46:16 -0500

    Implementado creacion de usuarios, encriptacion de contrasena con bycripjs y jwt

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,16 @@
 const { response, request } = require("express");
-const User = require('../models/users.model')
+const jwt = require('jsonwebtoken');
+const User = require('../models/users.model');
+
 
 
 const getUser = async (req = request, res = response) => {
 
     try {
+        const users = await User.find()
         res.status(200).json({
-            msg: 'Lista de usuarios'
+            msg: 'Lista de usuarios',
+            users
         })
     } catch (error) {
         return res.status(500).json({ msg: error.message })
@@ -17,14 +21,26 @@ const getUser = async (req = request, res = response) => {
 const createUser = async (req = request, res = response) => {
 
     try {
-        const body = req.body;
-        const user = new User(body);
-        console.log(body);
-        const userSaved = await user.save();
+        const { nombre, usuario, password, email } = req.body;
+        const newUser = new User({
+            nombre,
+            usuario,
+            password: await User.encryptPassword(password),
+            email
+        });
+
+        const savedUser = await newUser.save();
+        const username = savedUser.usuario;
+
+        const token = jwt.sign({ id: savedUser._id }, 'millavesecreta', {
+            expiresIn: 86400, //*24 Horas
+        });
+
 
         res.status(201).json({
             msg: 'Usuario registrado exitosamente!',
-            userSaved
+            username,
+            token
         })
     } catch (error) {
         return res.status(500).json({ msg: error.message })
